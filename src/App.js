7bb38babbbcef0b523151b2ad15f8f8cfa21c4b6
ramkrishna mondal <ramkrishnam170@gmail.com>
2024-01-21 11:49:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+import About from './components/About.js';
+import Sponsors from './components/Sponsors.js';
+import Location from './components/Location.js';
+import Registration from './components/Registration.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Sponsors/>
+    <Location/>
+    <Registration/>
+    <Footer/>
+    </>
   );
 }
 
